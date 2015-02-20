d81eb9994e06a2f05ba6b6b090dc91d4cc349aa4
mihanick <mihanick@gmail.com>
2015-02-20 23:00:06 +0600

    Created bat file to autoload assembly to nanocad

---
+++ NanoDwgCrawler/create_nApp.ini.bat
@@ -0,0 +1,2 @@
+echo [\PreLoadedModules]>>"C:\Program Files\Nanosoft\nanoCAD СПДС Железобетон 2.4\nApp.ini"
+echo NanoDwgCrawler.dll>>"C:\Program Files\Nanosoft\nanoCAD СПДС Железобетон 2.4\nApp.ini
