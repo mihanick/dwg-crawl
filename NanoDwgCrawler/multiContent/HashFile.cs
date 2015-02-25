36d675e070f382d231391cd70426e10d3ffb21d3
mihanick <mihanick@gmail.com>
2015-02-25 18:34:10 +0600

    Added File hashing function

---
+++ NanoDwgCrawler/multiContent/HashFile.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+namespace Crawl
+{
+    //http://stackoverflow.com/questions/2150455/how-do-i-create-an-md5-hash-digest-from-a-text-file
+    static class UtilityHash
+    {
+        public static string HashFile(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return HashFile(fs);
+            }
+        }
+
+        public static string HashFile(FileStream stream)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (stream != null)
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+
+                MD5 md5 = MD5CryptoServiceProvider.Create();
+                byte[] hash = md5.ComputeHash(stream);
+                foreach (byte b in hash)
+                    sb.Append(b.ToString("x2"));
+
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
