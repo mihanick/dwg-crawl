61a8774458ba200220d5ff8cfee0265dd4c3add4
mihanick <mihanick@gmail.com>
2015-04-01 17:25:18 +0600

    Added copying files from source path to local folder on scanning

--- ScanApp/Program.cs
+++ ScanApp/Program.cs
@@ -14,8 +14,9 @@ namespace Crawl
         static void Main(string[] args)
         {
             //Открыть папку, выбрать все файлы двг из нее
-            string dir = @"C:\svn\Help\MCS10\OgpUtils\OgpUtils\multiContent\testSamples";
-            //string dir = @"\\FILESERVER\home\#АРХИВ 2014";
+            //string dir = @"C:\svn\Help\MCS10\OgpUtils\OgpUtils\multiContent\testSamples";
+            string dir = @"\\FILESERVER\home\#АРХИВ 2014\Объекты\МНОГОТОПЛИВНАЯ АЗС №15";
+            string dataDir = @"F:\Data\";
 
             string[] dwgFiles = Directory.GetFiles(dir, "*.dwg", SearchOption.AllDirectories);
             SqlDb sqlDB = new SqlDb();
@@ -23,6 +24,7 @@ namespace Crawl
             foreach (string dwgFile in dwgFiles)
             {
                 CrawlDocument cDoc = new CrawlDocument(dwgFile);
+                FileCopy(dwgFile, Path.Combine(dataDir,cDoc.FileId+".dwg"));
                 sqlDB.InsertIntoFiles(cDoc.Path, cDoc.docJson, cDoc.FileId, cDoc.Hash);
             }
 
@@ -36,7 +38,7 @@ namespace Crawl
             }
 
             //Процесс выбирает из базы случайным образом непросканированный файл и сканирует его в Json
-
+            //Это пока выполняется вручным запуском нанокадов
             //Если файл изменился, то записывается его новый hash
         }
 
@@ -61,6 +63,62 @@ namespace Crawl
 
             p.Start();
             return p;
+        }
+
+        /// <summary>
+        /// Copies file overwriting and creating path if necessary
+        /// </summary>
+        /// <param name="sourceFullPath">Full source path</param>
+        /// <param name="targetFullPath">Full destination path</param>
+        [DebuggerStepThrough]
+        private static void FileCopy(string sourceFullPath, string targetFullPath)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(sourceFullPath);
+
+                //http://msdn.microsoft.com/en-us/library/cc148994.aspx
+
+                // Use Path class to manipulate file and directory paths. 
+                string targetDirectory = Path.GetDirectoryName(targetFullPath);
+                // To copy a folder's contents to a new location: 
+                // Create a new target folder, if necessary. 
+                if (!System.IO.Directory.Exists(targetDirectory))
+                {
+                    System.IO.Directory.CreateDirectory(targetDirectory);
+                }
+
+                // To copy a file to another location and  
+                // overwrite the destination file if it already exists.
+                System.IO.File.Copy(sourceFullPath, targetFullPath, true);
+
+                Console.WriteLine("Скопирован файл из {0} в {1}",sourceFullPath, targetFullPath);
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("[ОШИБКА] Ошибка копирования файла {0} в файл {1} '{2}'", sourceFullPath, targetFullPath, e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Clears ReadOnly attribute from target File
+        /// </summary>
+        /// <param name="targetFilePath">Full Path to file</param>
+        private static void ClearReadOnlyAttribute(string targetFilePath)
+        {
+            //https://msdn.microsoft.com/en-us/library/system.io.file.setattributes%28v=vs.110%29.aspx
+            try
+            {
+                FileAttributes fileAttributes = File.GetAttributes(targetFilePath);
+                fileAttributes = fileAttributes & ~FileAttributes.ReadOnly;
+                File.SetAttributes(targetFilePath, fileAttributes);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("[ОШИБКА] Ошибка задания атрибутов файла {0}: '{1}'", targetFilePath, e.Message);
+            }
         }
     }
 }
