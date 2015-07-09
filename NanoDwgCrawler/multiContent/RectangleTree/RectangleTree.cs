efca33eb13dc4a1092372abff3362e70e103a7ef
mihanick <mihanick@gmail.com>
2015-07-09 12:26:42 +0600

    Added implementation for checking structure and finding intersections in rectangle tree

--- NanoDwgCrawler/multiContent/RectangleTree/RectangleTree.cs
+++ NanoDwgCrawler/multiContent/RectangleTree/RectangleTree.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Crawl;
-
-namespace Crawl
+﻿namespace Crawl
 {
+    using System;
+    using System.Collections.Generic;
+
     public class RectangleTree
     {
         public Node Root { get; set; }
@@ -14,6 +10,9 @@ namespace Crawl
         #region Internal Classes
         public class Node
         {
+            #region properties
+            public bool IsALeaf;
+
             private Rectangle contents;
             public Rectangle Contents
             {
@@ -21,40 +20,6 @@ namespace Crawl
                 {
                     return this.contents;
                 }
-            }
-
-            private Rectangle boundBox;
-            public Rectangle BoundBox
-            {
-                get
-                {
-                    return this.boundBox;
-                }
-            }
-
-            public Node()
-            {
-                this.boundBox = new Rectangle();
-                this.children = new List<Node>();
-                this.boundBox = new Rectangle();
-            }
-
-            public Node(Rectangle rec)
-            {
-                this.contents = rec;
-                this.boundBox = rec.Clone();
-                this.children = new List<Node>();
-            }
-
-            public Node(Node nodeLeft, Node nodeRight)
-            {
-                this.children = new List<Node>();
-                this.children.Add(nodeLeft);
-                this.children.Add(nodeRight);
-
-                this.contents = null;
-                this.boundBox = new RectangleIntersection(nodeLeft.boundBox, nodeRight.boundBox);
-
             }
 
             private List<Node> children;
@@ -66,14 +31,62 @@ namespace Crawl
                 }
             }
 
+            private Rectangle boundBox;
+            public Rectangle BoundBox
+            {
+                get
+                {
+                    return this.boundBox;
+                }
+            }
+            #endregion
+
+            #region Constructors
+            public Node()
+            {
+                this.IsALeaf = false;
+                this.boundBox = new Rectangle();
+                this.children = new List<Node>();
+                this.boundBox = new Rectangle();
+            }
+
+            public Node(Rectangle rec)
+            {
+                this.IsALeaf = true;
+                this.contents = rec;
+                this.boundBox = rec.Clone();
+                this.children = new List<Node>();
+            }
+
+            public Node(Node nodeLeft, Node nodeRight)
+            {
+                this.IsALeaf = false;
+                this.children = new List<Node>();
+                this.children.Add(nodeLeft);
+                this.children.Add(nodeRight);
+
+                this.contents = null;
+                this.boundBox = new RectangleIntersection(nodeLeft.boundBox, nodeRight.boundBox);
+
+            }
+            #endregion
+
+            #region Methods
             public void AddChild(Node node)
             {
+                if (node == null)
+                    throw new ArgumentNullException();
+
+                if (this.IsALeaf)
+                    throw new System.Exception(" Cannot add child to a leaf");
+
                 foreach (Node child in this.children)
-                    if (child.Contains(node.boundBox))
-                    {
-                        child.AddChild(node);
-                        return;
-                    }
+                    if (!child.IsALeaf)
+                        if (child.Contains(node.boundBox))
+                        {
+                            child.AddChild(node);
+                            return;
+                        }
 
                 this.children.Add(node);
                 RectangleIntersection intersection = new RectangleIntersection(this.boundBox, node.BoundBox);
@@ -82,7 +95,7 @@ namespace Crawl
 
             public bool Contains(Rectangle rec)
             {
-                if (this.boundBox.Includes(rec))
+                if (this.boundBox.Intersects(rec))
                     return true;
 
                 return false;
@@ -111,7 +124,7 @@ namespace Crawl
                 if (this.contents == null)
                 {
                     foreach (Node child in this.children)
-                        result.AddRange(child.Included(rec));
+                        result.AddRange(child.Intersected(rec));
                 }
                 else
                     if (rec.Intersects(this.boundBox))
@@ -119,31 +132,53 @@ namespace Crawl
 
                 return result;
             }
+
+            public override string ToString()
+            {
+                return this.ConvertToString();
+            }
+
+            public string ConvertToString(string indent = "")
+            {
+                string result = indent + this.boundBox.ToString() + Environment.NewLine;
+                foreach (Node child in this.children)
+                    result += child.ConvertToString(indent + "\t") + Environment.NewLine;
+                if (this.IsALeaf)
+                    result += indent+"\t" + "rec:" + this.contents.ToString();
+                return result;
+            }
+
+            public List<Rectangle> GetContents()
+            {
+                List<Rectangle> result = new List<Rectangle>();
+                foreach (Node child in this.children)
+                    result.AddRange(child.GetContents());
+
+                if (this.contents != null)
+                    result.Add(this.contents);
+
+                return result;
+            }
+            #endregion
         }
 
         #endregion
 
         public RectangleTree()
         {
+            this.Root = new Node();
         }
 
         #region Methods
         public void Add(Rectangle rec)
         {
-            if (this.Root == null)
-                this.Root = new Node(rec);
-            else
-                AddContents(rec);
-
+            AddContents(rec);
         }
 
         private void AddContents(Rectangle rec)
         {
-            if (this.Root == null)
-                throw new Exception("Нет корневого элемента");
-
             if (rec == null)
-                throw new Exception("Нельзя добавлять пустой прямоугольник");
+                throw new NullReferenceException("Нельзя добавлять пустой прямоугольник");
 
             // Если добавляемый прямоугольник не содержится в корневом элементе
             if (!this.Root.Contains(rec))
@@ -168,100 +203,9 @@ namespace Crawl
             return this.Root.Intersected(rec);
         }
 
-        #endregion
-    }
-
-    public class ClusterTree
-    {
-        private RectangleTree rTree;
-
-        public int Depth { get; set; }
-
-        #region Internal Classes
-
-        public class Cluster : HashSet<Rectangle>
+        public List<Rectangle> GetContents()
         {
-            #region properties
-            public int Level { get; set; }
-
-            private Rectangle boundBox;
-            public Rectangle BoundBox
-            {
-                get
-                {
-                    return this.boundBox;
-                }
-            }
-            #endregion
-
-            public Cluster()
-            {
-                this.Level = -1;
-                this.boundBox = new Rectangle();
-            }
-
-            #region Methods
-
-            public void Add(Rectangle rec)
-            {
-                base.Add(rec);
-                RectangleIntersection intersection = new RectangleIntersection(this.boundBox, rec);
-                this.boundBox = intersection;
-            }
-            #endregion
-        }
-        #endregion
-
-        public ClusterTree(Rectangle[] rects)
-        {
-            List<Rectangle> rectangles = new List<Rectangle>(rects);
-            foreach (var rec in rects)
-                this.rTree.Add(rec);
-
-            this.FindClusters(rectangles);
-        }
-
-        private void FindClusters(List<Rectangle> rectangles)
-        {
-            List<Cluster> clusters = new List<Cluster>();
-
-            List<Rectangle> iteratedList = new List<Rectangle>(rectangles);
-
-            while (iteratedList.Count != 0)
-            {
-                iteratedList = iterate(iteratedList);
-            }
-        }
-
-        private List<Rectangle> iterate(List<Rectangle> rectangles)
-        {
-            List<Rectangle> input = new List<Rectangle>(rectangles);
-            List<Cluster> clusters = new List<Cluster>();
-            List<Rectangle> toRemove = new List<Rectangle>();
-
-            foreach (Rectangle rec in input)
-            {
-                List<Rectangle> intersections = rTree.Intersections(rec);
-                toRemove.AddRange(intersections);
-
-                Cluster cluster = new Cluster();
-                foreach (Rectangle rectangleIntersected in intersections)
-                    cluster.Add(rectangleIntersected);
-
-                clusters.Add(cluster);
-            }
-
-            foreach (Rectangle rect in toRemove)
-                input.Remove(rect);
-
-            return input;
-        }
-
-        #region Methods
-
-        public List<Cluster> Clusters(int level = 0)
-        {
-            throw new NotImplementedException();
+            return this.Root.GetContents();
         }
         #endregion
     }
