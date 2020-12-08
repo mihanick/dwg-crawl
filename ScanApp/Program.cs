7690ed09382165b5e3905e7efc5254fc24ef9f65
mihanick <mihanick@gmail.com>
2020-12-08 10:22:34 +0600

    Why am I still removing stuff??

--- ScanApp/Program.cs
+++ ScanApp/Program.cs
@@ -1,47 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using Crawl;
+﻿using DwgDump;
+using DwgDump.Db;
+using System;
 //using System.Threading.Tasks;
 using System.Diagnostics;
-using DwgDump.Db;
-using DwgDump;
+using System.IO;
 
-namespace Crawl
+namespace DwgDump
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //string dir = @"C:\svn\Help\MCS10\OgpUtils\OgpUtils\multiContent\testSamples";
-            string dir = @"U:\#АРХИВ 2014\Объекты";
-            //string dir = @"D:\Documents\Desktop\rectangles";
-            //string dir = @"D:\Documents\Desktop\SingleFile";
-            //string dir = @"D:\Documents\Dropbox\CrawlDwgs";
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string dir = @"C:\git\dwg-crawl\TestData";
 
-            string dbName = "geometry";
+			string dbName = "geometry";
 
-            Scan(dir,dbName);
-        }
+			Scan(dir, dbName);
+		}
 
-        static void Scan(string dir, string dbName)
-        {
-            //Открыть папку, выбрать все файлы двг из нее
-            string dataDir = @"c:\Data\";
+		static void Scan(string dir, string dbName)
+		{
+			//Открыть папку, выбрать все файлы двг из нее
+			string dataDir = @"C:\git\dwg-crawl\Data";
 
-            string[] dwgFiles = Directory.GetFiles(dir, "*.dwg", SearchOption.AllDirectories);
-            DbMongo db = new DbMongo(dbName);
+			string[] dwgFiles = Directory.GetFiles(dir, "*.dwg", SearchOption.AllDirectories);
+			DbMongo db = new DbMongo(dbName);
 
-            foreach (string dwgFile in dwgFiles)
-            {
-                CrawlDocument cDoc = new CrawlDocument(dwgFile);
-                FileCopy(dwgFile, Path.Combine(dataDir, cDoc.FileId + ".dwg"));
-                db.InsertIntoFiles(cDoc);
-            }
+			foreach (string dwgFile in dwgFiles)
+			{
+				CrawlDocument cDoc = new CrawlDocument(dwgFile);
+				FileCopy(dwgFile, Path.Combine(dataDir, cDoc.FileId + ".dwg"));
+				db.InsertIntoFiles(cDoc);
+			}
 
-            /*
+			/*
             //Запуситить процессы по числу ядер процессоров каждый на своем ядре
             int numCores = 4;
             for (int i = 0; i < numCores; i++)
@@ -54,85 +46,84 @@ namespace Crawl
             //Если файл изменился, то записывается его новый hash
             }
             */
-        }
+		}
 
-        static void crawlinNano()
-        {
-            ExecuteCommandLine(@"C:\Program Files (x86)\Nanosoft\nanoCAD СПДС Железобетон 2.4\nCad.exe");
-        }
+		static void crawlinNano()
+		{
+			ExecuteCommandLine(@"C:\Program Files (x86)\Nanosoft\nanoCAD СПДС Железобетон 2.4\nCad.exe");
+		}
 
-        static Process ExecuteCommandLine(string exePath, string arguments = "")
-        {
+		static Process ExecuteCommandLine(string exePath, string arguments = "")
+		{
+			//http://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results
+			// Start the child process.
+			Process p = new Process();
+			// Redirect the output stream of the child process.
+			//p.StartInfo.UseShellExecute = false;
+			//p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.FileName = exePath;
 
-            //http://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results
-            // Start the child process.
-            Process p = new Process();
-            // Redirect the output stream of the child process.
-            //p.StartInfo.UseShellExecute = false;
-            //p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.FileName = exePath;
+			//http://social.msdn.microsoft.com/Forums/vstudio/en-US/1bbf1e4c-1911-40a4-861e-2b2990124314/execute-command-line-in-c
+			p.StartInfo.Arguments = arguments;
 
-            //http://social.msdn.microsoft.com/Forums/vstudio/en-US/1bbf1e4c-1911-40a4-861e-2b2990124314/execute-command-line-in-c
-            p.StartInfo.Arguments = arguments;
+			p.Start();
+			return p;
+		}
 
-            p.Start();
-            return p;
-        }
+		/// <summary>
+		/// Copies file overwriting and creating path if necessary
+		/// </summary>
+		/// <param name="sourceFullPath">Full source path</param>
+		/// <param name="targetFullPath">Full destination path</param>
+		[DebuggerStepThrough]
+		private static void FileCopy(string sourceFullPath, string targetFullPath)
+		{
+			try
+			{
+				string fileName = Path.GetFileName(sourceFullPath);
 
-        /// <summary>
-        /// Copies file overwriting and creating path if necessary
-        /// </summary>
-        /// <param name="sourceFullPath">Full source path</param>
-        /// <param name="targetFullPath">Full destination path</param>
-        [DebuggerStepThrough]
-        private static void FileCopy(string sourceFullPath, string targetFullPath)
-        {
-            try
-            {
-                string fileName = Path.GetFileName(sourceFullPath);
+				//http://msdn.microsoft.com/en-us/library/cc148994.aspx
 
-                //http://msdn.microsoft.com/en-us/library/cc148994.aspx
+				// Use Path class to manipulate file and directory paths. 
+				string targetDirectory = Path.GetDirectoryName(targetFullPath);
+				// To copy a folder's contents to a new location: 
+				// Create a new target folder, if necessary. 
+				if (!System.IO.Directory.Exists(targetDirectory))
+				{
+					System.IO.Directory.CreateDirectory(targetDirectory);
+				}
 
-                // Use Path class to manipulate file and directory paths. 
-                string targetDirectory = Path.GetDirectoryName(targetFullPath);
-                // To copy a folder's contents to a new location: 
-                // Create a new target folder, if necessary. 
-                if (!System.IO.Directory.Exists(targetDirectory))
-                {
-                    System.IO.Directory.CreateDirectory(targetDirectory);
-                }
+				// To copy a file to another location and  
+				// overwrite the destination file if it already exists.
+				System.IO.File.Copy(sourceFullPath, targetFullPath, true);
 
-                // To copy a file to another location and  
-                // overwrite the destination file if it already exists.
-                System.IO.File.Copy(sourceFullPath, targetFullPath, true);
+				Console.WriteLine("Скопирован файл из {0} в {1}", sourceFullPath, targetFullPath);
 
-                Console.WriteLine("Скопирован файл из {0} в {1}",sourceFullPath, targetFullPath);
-
-            }
-            catch (System.Exception e)
-            {
-                Console.WriteLine("[ОШИБКА] Ошибка копирования файла {0} в файл {1} '{2}'", sourceFullPath, targetFullPath, e.Message);
-            }
-        }
+			}
+			catch (System.Exception e)
+			{
+				Console.WriteLine("[ОШИБКА] Ошибка копирования файла {0} в файл {1} '{2}'", sourceFullPath, targetFullPath, e.Message);
+			}
+		}
 
 
-        /// <summary>
-        /// Clears ReadOnly attribute from target File
-        /// </summary>
-        /// <param name="targetFilePath">Full Path to file</param>
-        private static void ClearReadOnlyAttribute(string targetFilePath)
-        {
-            //https://msdn.microsoft.com/en-us/library/system.io.file.setattributes%28v=vs.110%29.aspx
-            try
-            {
-                FileAttributes fileAttributes = File.GetAttributes(targetFilePath);
-                fileAttributes = fileAttributes & ~FileAttributes.ReadOnly;
-                File.SetAttributes(targetFilePath, fileAttributes);
-            }
-            catch (System.Exception e)
-            {
-                Console.WriteLine("[ОШИБКА] Ошибка задания атрибутов файла {0}: '{1}'", targetFilePath, e.Message);
-            }
-        }
-    }
+		/// <summary>
+		/// Clears ReadOnly attribute from target File
+		/// </summary>
+		/// <param name="targetFilePath">Full Path to file</param>
+		private static void ClearReadOnlyAttribute(string targetFilePath)
+		{
+			//https://msdn.microsoft.com/en-us/library/system.io.file.setattributes%28v=vs.110%29.aspx
+			try
+			{
+				FileAttributes fileAttributes = File.GetAttributes(targetFilePath);
+				fileAttributes = fileAttributes & ~FileAttributes.ReadOnly;
+				File.SetAttributes(targetFilePath, fileAttributes);
+			}
+			catch (System.Exception e)
+			{
+				Console.WriteLine("[ОШИБКА] Ошибка задания атрибутов файла {0}: '{1}'", targetFilePath, e.Message);
+			}
+		}
+	}
 }
