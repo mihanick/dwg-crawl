1148aca8a1d073c7b60c183e86384494081af39c
mihanick <mihanick@gmail.com>
2021-02-05 17:05:08 +0600

    Setup training script

--- JupyterNotebooks/train.py
+++ JupyterNotebooks/train.py
@@ -1,28 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 import time
 
+def calculate_accuracy(dim_outputs, y):
+    '''
+    For now we will calculate accuracy by model guessing the right amount of dimensions produced
+    '''
+
+    accuracies = []
+
+    # y, dim_outputs: list len=batch_size of tensors with shape (dim_count, dim_features)
+    for batch_no in range(len(y)):
+        actual_dim_number = y[batch_no].shape[0]
+        predicted_dim_number = dim_outputs[batch_no].shape[0]
+        acc = 0
+        if actual_dim_number == 0:
+            acc = np.abs(actual_dim_number - predicted_dim_number)
+        else:
+            acc = np.abs(actual_dim_number - predicted_dim_number) / actual_dim_number
+        accuracies.append(acc)
+
+    return np.mean(accuracies)
+
 def plot_history(loss_history, train_history, val_history):
+    '''
+    Plots learning history in jupyter
+    '''
     plt.ylabel('Accuracy @ epoch')
     
     train, = plt.plot(train_history)
     train.set_label("train")
+
     validation, = plt.plot(val_history)
     validation.set_label("validation")
+
+    loss, = plt.plot(loss_history)
+    loss.set_label("loss")
 
     plt.legend()
     plt.show()
     
 def train_model(encoder, decoder, train_loader, val_loader, loss, decoder_opt,encoder_opt, epochs):
+    '''
+    trains model and outputs loss, train and validation history
+    '''
     start = time.time()
     
-    loss_history = []
-    train_history = []
-    val_history  = []
+    loss_history   = []
+    train_history  = []
+    val_history    = []
     train_accuracy = 0.0
-    val_accuracy = 0.0
+
     
     for epoch in range(epochs):
         encoder.train()
@@ -30,15 +58,14 @@ def train_model(encoder, decoder, train_loader, val_loader, loss, decoder_opt,en
         
         loss_accumulated = 0
         
-        i_step = 1
-        for _, (x,y) in enumerate(train_loader):
+        for i, (x, y) in enumerate(train_loader):
             
             decoder_opt.zero_grad()
             encoder_opt.zero_grad()
             
-            outs_num, learned = encoder(x)
+            outs_num, learned_hidden_representation = encoder(x)
             
-            decoded = decoder(learned, outs_num)
+            decoded = decoder(outs_num, learned_hidden_representation)
             loss_value = loss(decoded, y)
             
             loss_value.backward()
@@ -47,19 +74,32 @@ def train_model(encoder, decoder, train_loader, val_loader, loss, decoder_opt,en
             
             loss_accumulated += loss_value
             
-            #val_accuracy = compute_accuracy(model, val_loader)
-            print('[{0} @ {4:.1f} sec] Loss: {1:4f} Train err: {2:2.2f}% Val err: {3:2.2f}%'. format(
-                epoch,
-                loss_value.item(),
-                (1 - train_accuracy) * 100,
-                (1 - val_accuracy) * 100,
-                time.time() - start
-            ))
-        
-        # loss_history.append(float(loss_accum / i_step))
-        # val_history.append(val_accuracy)
-        # clear_output(wait=True)
-        
-        i_step += 1
+            train_accuracy = calculate_accuracy(decoded, y)
 
-    return loss_history, train_history, val_history        
+            print('[{0}-{1} @ {2:.1f} sec] Loss: {3:4f} Train err: {4:2.2f}%'. format(
+                epoch,
+                i,
+                time.time() - start,
+                loss_value.item(),
+                (1 - train_accuracy) * 100
+            ))
+
+        train_history.append(train_accuracy)
+        loss_history.append(float(loss_accumulated / epoch))
+
+        # validation
+        encoder.eval()
+        decoder.eval()
+
+        val_accuracies = []
+        for _, (x, y) in enumerate(val_loader):
+            outs, hiddens = encoder(x)
+            dim_predictions = decoder(outs, hiddens)
+
+            val_acc = calculate_accuracy(dim_predictions, y)
+            val_accuracies.append(val_acc)
+        val_accurcy = np.mean(val_accuracies)
+        print('Epoch validation accuracy:', val_accurcy)
+        val_history.append(val_accurcy)
+
+    return loss_history, train_history, val_history
