d10982cb85c05c140fff8d51efde228155be47bf
mihanick <mihanick@gmail.com>
2021-04-15 10:55:37 +0600

    Trying to teach against early eos

--- JupyterNotebooks/main.py
+++ JupyterNotebooks/main.py
@@ -12,24 +12,24 @@ from dataset import DwgDataset
 def run(batch_size=32, pickle_file='test_dataset_cluster_labeled.pickle', lr=0.008, epochs=5, train_verbose=True, limit_seq_len=200):
     dwg_dataset = DwgDataset(pickle_file=pickle_file, batch_size=batch_size, limit_seq_len=limit_seq_len)
     trainer = Trainer(
-        dwg_dataset, 
-        lr=lr, 
+        dwg_dataset,
+        lr=lr,
         train_verbose=train_verbose)
 
     train_kl_losses = []
     train_rl_losses = []
-    val_kl_losses  = []
-    val_rl_losses  = []
+    val_kl_losses   = []
+    val_rl_losses   = []
     
     for epoch in range(epochs):
-        test_rl, test_kl, val_rl, val_kl = trainer.train_epoch(epoch)
-        train_kl_losses.append(test_kl)
+        test_rl, train_kl, val_rl, val_kl = trainer.train_epoch(epoch)
+        train_kl_losses.append(train_kl)
         train_rl_losses.append(test_rl)
         val_kl_losses.append(val_kl)
         val_rl_losses.append(val_rl)
 
     # Calculate test accuracy
-    test_kl, test_rl = trainer.CalculateLoaderAccuracy(trainer.test_loader)
-    print('Test losses rl:{:1.4f} kl:{:1.4f}'.format(test_rl, test_kl))
+    train_kl, test_rl = trainer.CalculateLoaderAccuracy(trainer.test_loader)
+    print('Test losses rl:{:1.4f} kl:{:1.4f}'.format(test_rl, train_kl))
 
-    return train_rl_losses, train_kl_losses, val_rl_losses, val_kl_losses, test_rl, test_kl
+    return train_rl_losses, train_kl_losses, val_rl_losses, val_kl_losses, test_rl, train_kl
--- JupyterNotebooks/run.py
+++ JupyterNotebooks/run.py
@@ -2,4 +2,13 @@
 main runner from console
 '''
 from main import run
-run()
+# keep lr low, or lstm output will have NaNs
+# https://stackoverflow.com/questions/33962226/common-causes-of-nans-during-training
+
+run(
+    pickle_file="test_dataset_cluster_labeled_.pickle",
+    train_verbose=False,
+    batch_size=4,
+    limit_seq_len=60,
+    epochs=15,
+    lr=0.0001)
