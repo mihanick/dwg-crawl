fb35d2af26a69251565a56d3b7e146d93a2bfc89
mihanick <mihanick@gmail.com>
2021-09-17 11:09:39 +0600

    Add prediction command

--- NanoDwgCrawler/Prediction.cs
+++ NanoDwgCrawler/Prediction.cs
@@ -1,4 +1,5 @@
-﻿using Multicad;
+﻿using DwgDump.MultiContent;
+using Multicad;
 using Multicad.AplicationServices;
 using Multicad.DatabaseServices;
 using Multicad.DatabaseServices.StandardObjects;
@@ -22,60 +23,6 @@ namespace DwgDump
 	{
 
 		private static HttpClient client = null;
-
-		/// <summary>
-		/// Создает превью из формата размером <paramref name="imageSize"/> пикселей в формате png
-		/// </summary>
-		/// <param name="format"></param>
-		/// <returns></returns>
-		static private (Bitmap, BoundBlock) CreatePreview(List<McObjectId> formatEntIds, int imageSize = 512)
-		{
-			var bbOx = new BoundBlock();
-			foreach (var entId in formatEntIds)
-			{
-				McDbEntity dbe = (entId.GetObject())?.Cast<McDbEntity>();
-				if (dbe != null)
-					bbOx = bbOx.MergeWith(dbe.BoundingBox);
-			}
-
-			McEmfParams p = new McEmfParams()
-			{
-				UseColor = true,
-				UseWidth = true,
-				BgrColor = System.Drawing.Color.White,
-				PntOfLeftBtm = bbOx.BasePoint,
-				SizeX = bbOx.SizeByX,
-				SizeY = bbOx.SizeByY,
-				Frame = 3,
-				SquareEmf = false,
-				Quite = true,
-				Scale = 10
-			};
-
-			Metafile mf = McNativeGate.CreateEmf(formatEntIds, p);
-
-			int bmpHeight, bmpWidth;
-			if (mf.Height > mf.Width)
-			{
-				bmpHeight = imageSize;
-				bmpWidth = (int)(mf.Width * imageSize / mf.Height);
-			}
-			else
-			{
-				bmpWidth = imageSize;
-				bmpHeight = (int)(mf.Height * imageSize / mf.Width);
-			}
-
-			Bitmap d = new Bitmap(mf, bmpWidth, bmpHeight);
-
-			using (Bitmap b = new Bitmap(d))
-			{
-				// string path = Path.Combine(Path.GetTempFileName() + ".png");
-				//b.Save(path, ImageFormat.Png);
-			}
-
-			return (d, bbOx);
-		}
 
 		// https://stackoverflow.com/questions/1131425/send-a-file-via-http-post-with-c-sharp
 		private static async Task<string> Upload(string actionUrl, string paramString, Stream paramFileStream, byte[] paramFileBytes)
@@ -272,7 +219,7 @@ namespace DwgDump
 				currentSelection = McObjectManager.SelectObjects("Select fragment").ToList();
 
 			// write image
-			(var image, var bound) = CreatePreview(currentSelection);
+			(var image, var bound) = BmpFromDwg.CreatePreview(currentSelection);
 
 			string json = PostImage(image);
 
@@ -314,7 +261,7 @@ namespace DwgDump
 			foreach (var param in predictions)
 			{
 				List<McLinearDimension> dims = new List<McLinearDimension>();
-				if (param.PredictionClass == 1)
+				if (param.PredictionClass == 0)
 				{
 					var pnts = param.Bound.Get2dCorners();
 					var relBoundVec = (allEntitiesBound.Bound.Center - param.Bound.Center);
@@ -367,7 +314,10 @@ namespace DwgDump
 				// DrawPlineOverBound(param);
 
 				foreach (McLinearDimension dim in dims)
+				{
+					dim.DbEntity.Color = Color.DarkOrange;
 					dim.DbEntity.AddToCurrentDocument();
+				}
 			}
 
 		}
